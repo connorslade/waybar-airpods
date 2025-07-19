2cbc1ed6700a2cb730471119c19d82aca87f5ee7
Connor Slade <connor@connorcode.com>
2025-07-18 20:39:48 -0400

    Rewrite with bluer

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,65 @@
-use std::collections::HashSet;
-
 use anyhow::Result;
-use btleplug::{
-    api::{Central, CentralEvent, Manager as _, Peripheral, ScanFilter},
-    platform::Manager,
-};
+use bluer::rfcomm::{Profile, Role};
 use futures::StreamExt;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use uuid::{Uuid, uuid};
 
 const AIRPODS_SERVICE: Uuid = uuid!("74ec2172-0bad-4d01-8f77-997b2be0722a");
 
+const HANDSHAKE: &[u8] = &[0, 0, 4, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let manager = Manager::new().await?;
-    let adapter = &manager.adapters().await?[0];
+    let session = bluer::Session::new().await?;
+    let adapter = session.default_adapter().await?;
+    adapter.set_powered(true).await?;
 
-    let mut events = adapter.events().await?;
-    adapter.start_scan(ScanFilter::default()).await?;
+    let device = get_airpods(&adapter).await?.unwrap();
 
-    let mut connected = HashSet::new();
+    println!("Found AirPods: {:?}", device.all_properties().await?);
 
-    while let Some(event) = events.next().await {
-        // for id in connected.iter() {
-        //     let peripheral = adapter.peripheral(&id).await?;
-        //     println!("> {:?}", peripheral.properties().await.unwrap());
-        // }
+    // device.connect().await?;
+    // device.connect_profile(&AIRPODS_SERVICE).await?;
 
-        match event {
-            CentralEvent::DeviceDiscovered(id) => {
-                let peripheral = adapter.peripheral(&id).await?;
+    let profile = Profile {
+        uuid: AIRPODS_SERVICE,
+        role: Some(Role::Client),
+        require_authentication: Some(false),
+        require_authorization: Some(false),
+        auto_connect: Some(false),
+        ..Default::default()
+    };
 
-                let props = peripheral.properties().await?.unwrap();
-                if !props.services.contains(&AIRPODS_SERVICE) {
-                    continue;
-                }
+    let mut profile = session.register_profile(profile).await?;
+    let handle = profile.next().await.unwrap();
+    println!("Registered profile");
 
-                peripheral.connect().await.unwrap();
-                peripheral.discover_services().await.unwrap();
-                dbg!(adapter.peripheral(&id).await?.services());
+    let mut stream = handle.accept().unwrap();
+    println!("Stream created");
 
-                println!("Connected {id}: {:?}", props);
-                connected.insert(id);
-            }
+    stream.write_all(HANDSHAKE).await.unwrap();
 
-            ref x @ (CentralEvent::DeviceConnected(ref id)
-            | CentralEvent::DeviceUpdated(ref id)
-            | CentralEvent::DeviceDisconnected(ref id)
-            | CentralEvent::ManufacturerDataAdvertisement { ref id, .. }
-            | CentralEvent::ServiceDataAdvertisement { ref id, .. }
-            | CentralEvent::ServicesAdvertisement { ref id, .. })
-                if connected.contains(id) =>
-            {
-                dbg!(x);
-            }
-            _ => {}
+    loop {
+        let mut buffer = vec![0; 1024];
+        let bytes = stream.read(&mut buffer).await?;
+
+        println!("Received data: {:?}", &buffer[..bytes]);
+    }
+}
+
+async fn get_airpods(adapter: &bluer::Adapter) -> Result<Option<bluer::Device>> {
+    let connected = adapter.device_addresses().await?;
+    for connected in connected {
+        let device = adapter.device(connected)?;
+        if is_airpods(&device).await {
+            return Ok(Some(device));
         }
     }
 
-    Ok(())
+    Ok(None)
+}
+
+async fn is_airpods(device: &bluer::Device) -> bool {
+    let uuids = device.uuids().await.unwrap().unwrap();
+    uuids.contains(&AIRPODS_SERVICE)
 }
